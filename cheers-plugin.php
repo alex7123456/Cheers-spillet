3775c23b1bf615f403ebe02ee5f0a661ae049af7
alex7123456 <44665244+alex7123456@users.noreply.github.com>
2019-10-30 17:10:46 +0100

    Add files via upload

--- cheers-plugin.php
+++ cheers-plugin.php
@@ -81,4 +81,56 @@ function cheers_display_footer_scripts()
 }
 add_action('wp_footer','cheers_display_header_scripts');
 
+
+function cheers_form()
+{
+/* indholds variable */
+$content = '';
+$content .= '<form method="post" action="https://cheers1.achenza.dk/?page_id=650">';
+  $content .= '<input type="text" name="full_name" placeholder="Navn" />';
+  $content .= '<br />'; 
+
+  $content .= '<input type="text" name="email_address" placeholder="Email" />';
+  $content .= '<br />'; 
+
+  $content .= '<input type="text" name="phone_number" placeholder="Tlf." />';
+  $content .= '<br />'; 
+
+  $content .= '<textarea name="comments" placeholder="Send os en besked"></textarea>';
+  $content .= '<br />'; 
+
+  $content .= '<input type="submit" name="cheers_submit_form" value="Indsend" />';
+
+ $content .= '</form>'; 
+return $content;
+}
+add_shortcode('cheers_contact_form','cheers_form');
+
+
+function set_html_content_type()
+{
+return 'text/html';
+
+}
+
+function cheers_form_capture()
+{
+if (array_key_exists('cheers_submit_form',$_POST))
+{
+  $to = "asa@koanmedia.dk";
+  $subject = "Cheers form submission";
+  $body = '';
+  $body .= 'Name: '.$_POST['full_name'].' <br /> ';
+  $body .= 'Email: '.$_POST['email_address'].' <br /> ';
+  $body .= 'Tlf: '.$_POST['phone_number'].' <br /> ';
+  $body .= 'Comment: '.$_POST['comments'].' <br /> ';
+
+add_filter('wp_mail_content_type','set_html_content_type');
+  wp_mail($to,$subject,$body);
+  remove_filter('wp_mail_content_type','set_html_content_type');
+}
+
+
+}
+add_action('wp_head','cheers_form_capture');
 ?>
